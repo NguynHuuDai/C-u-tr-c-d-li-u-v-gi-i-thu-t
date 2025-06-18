f9b452a271e3963348327a5c6f677edc5197ac3b
daizzt10@gmail.com <daizzt10@gmail.com>
2025-06-18 23:26:22 +0700

    add

--- Session2/bai5.c
+++ Session2/bai5.c
@@ -30,13 +30,13 @@ int main()
     {
         arr[i] = arr[i + 1];
     }
+    n--;
     printf("Mang sau khi xoa la");
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
 
-
-
+    free(arr);
     return 0;
 }
